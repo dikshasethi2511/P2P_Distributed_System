8af94202eb02194a416ba1ccb433ddb53b38e30f
Diksha Sethi <dikshasethi251@gmail.com>
2024-05-22 16:45:22 +0530

    chore: changed reading from .env

--- communication/setup_peer.py
+++ communication/setup_peer.py
@@ -1,12 +1,15 @@
 import grpc
 import sys
+import os
+from dotenv import load_dotenv
 
+# Append the paths to sys.path
 sys.path.append("../proto")
 sys.path.append("../classes")
+
 from peer import Peer
 from threading import Thread, Event
 from concurrent import futures
-from dotenv import load_dotenv
 
 
 def run_peer(bootstrap_server_address):
@@ -21,7 +24,15 @@ def run_peer(bootstrap_server_address):
     peer.connect_to_bootstrap_server(CPU, RAM, storage)
 
 
-
 if __name__ == "__main__":
-    bootstrap_server_address = "localhost:50051"
+    # Load environment variables from .env file
+    load_dotenv()
+
+    # Get the bootstrap server address from the environment variables
+    bootstrap_server_address = os.getenv("BOOTSTRAP_SERVER_ADDRESS")
+
+    if bootstrap_server_address is None:
+        print("BOOTSTRAP_SERVER_ADDRESS is not set in the .env file")
+        sys.exit(1)
+
     run_peer(bootstrap_server_address)
