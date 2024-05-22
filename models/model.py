933bff73407e120710aee4e0242badf5dacaec26
Diksha Sethi <dikshasethi251@gmail.com>
2024-05-23 00:51:05 +0530

    fixed port thingy

--- models/model.py
+++ models/model.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import joblib
 
-csv_file = "../compute/PORT/Iris.csv"
+csv_file = "../compute/" + str(PORT) + "/Iris.csv"
 iris_data = pd.read_csv(csv_file)
 
 # Separate features and target variable
@@ -12,9 +12,9 @@ X = iris_data.drop(columns=["Id", "Species"])
 y = iris_data["Species"]
 
 # Load the initial KNN model
-initial_model_file = "../compute/PORT/initial_knn_model.joblib"
+initial_model_file = "../compute/" + str(PORT) + "/initial_knn_model.joblib"
 knn_model = joblib.load(initial_model_file)
 knn_model.fit(X, y)
 
 # Save the trained model to a file
-joblib.dump(knn_model, "../compute/PORT/weights.joblib")
+joblib.dump(knn_model, "../compute/" + str(PORT) + "/weights.joblib")
